c930529fb8c7a90341737fe5eafd324d4d3ab7ba
chuangk88 <77218739+chuangk88@users.noreply.github.com>
2025-08-17 21:48:33 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,15 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open("daimoku-cache").then(cache => {
       return cache.addAll(["index.html", "manifest.json"]);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
